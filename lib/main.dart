73a0034a637f9c30cd97886033b5757b576790d9
suraj <surajkumarsahani1997@gmail.com>
2021-05-21 09:15:51 +0530

    text changes

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Safe Outs Business'),
+      home: MyHomePage(title: 'Safe Outs'),
     );
   }
 }
