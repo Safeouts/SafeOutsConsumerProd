a7cd2f66722a16b4699421b9208facc50a9a29ab
suraj <surajkumarsahani1997@gmail.com>
2021-05-21 19:36:40 +0530

    Revert "consumer side setup v3..."
    This reverts commit 3448f318cfd27beb7dffefb5604649b978fb77f8.

--- lib/main.dart
+++ lib/main.dart
@@ -1,95 +1,45 @@
 import 'package:flutter/material.dart';
-import 'package:safeoutsconsumerprod/screens/splashscreen.dart';
 
 void main() {
-  WidgetsFlutterBinding.ensureInitialized();
-  // Firebase.initializeApp();
   runApp(MyApp());
 }
 
-
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-  final ThemeData _theme = _buildTheme();
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: _theme,
-
-      home: SplashScreen(),
-      debugShowCheckedModeBanner: false,
+      title: 'Safe Outs',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Safe Outs'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  // MyHomePage({Key key, this.title}) : super(key: key);
+  MyHomePage({Key? key, required this.title}) : super(key: key);
 
-  // final String title;
+  final String title;
 
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-
-      _counter++;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
 
     return Scaffold(
       appBar: AppBar(
-
+        title: Text(widget.title),
       ),
       body: Center(
-
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
-}
-ThemeData _buildTheme() {
-  final ThemeData base = ThemeData.light();
-  return base.copyWith(
-    primaryColor: Colors.white,
-    primaryColorBrightness: Brightness.light,
-    primaryTextTheme: Typography.blackMountainView,
-    primaryIconTheme: const IconThemeData(
-      color: Colors.grey,
-    ),
-    accentColor: Colors.green[800],
-    buttonTheme: base.buttonTheme.copyWith(
-      buttonColor: Colors.green[800],
-      textTheme: ButtonTextTheme.primary,
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(4),
-      ),
-    ),
-    scaffoldBackgroundColor: Colors.white,
-  );
 }
